985da1cd5510f0313f61ef274d0e1d0cc54c255f
jannekesn <janneke-95@hotmail.com>
2019-01-30 18:51:44 +0100

    Updates

---
+++ precache-manifest.5a249652afc6027333e1c985534ef333.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/workshop/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a66ac1f97ff004b272d2",
+    "url": "/workshop/static/js/runtime~main.a66ac1f9.js"
+  },
+  {
+    "revision": "793d44060a1c02ab1942",
+    "url": "/workshop/static/js/main.793d4406.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/workshop/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "793d44060a1c02ab1942",
+    "url": "/workshop/static/css/main.05e9141e.chunk.css"
+  },
+  {
+    "revision": "45501515bd57e71c6ef9af3816a630f0",
+    "url": "/workshop/index.html"
+  }
+];
